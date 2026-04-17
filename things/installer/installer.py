a182e6245fd5365130d4958503541b59e3032af9
Coxinha <00001216166821sp@al.educacao.sp.gov.br>
2026-04-17 20:45:47 -0300

    Update installer.py
    Forgot to update installer to use versions.json

--- things/installer/installer.py
+++ things/installer/installer.py
@@ -3,8 +3,10 @@ print("Project16 Installer")
 username = input("[?] Username for Project16: ")
 print("[i] Starting install process...")
 base = "https://raw.githubusercontent.com/Coxinha2K/Project16/main"
+print("[i] Downloading version list...")
+vlist = json.loads(requests.get(f"{base}/versions.json").text)
 print("[i] Downloading RetroFS...")
-rfs_code = requests.get(f"{base}/things/retrofs/retrofs_alpha.py").text
+rfs_code = requests.get(f"{base}/{vlist['RetroFS']['Latest']['Path']}").text
 print("[i] Loading RetroFS...")
 name = "rfs"
 rfs = types.ModuleType(name)
@@ -18,13 +20,19 @@ fs.format("P1", "Project16")
 fs.format("P2", "RAM")
 
 print("[i] Install step 3 - installing Core16")
-core16 = requests.get(f"{base}/things/core16/core16_a10_se.py").text
-
+core16 = requests.get(f"{base}/{vlist['Core16']['Latest']['Path']}").text
+# core16 = open("stuff/core16.py", "r").read()
 fs.mkdir("P0", "/system")
 fs.mkdir("P0", "/system/config")
 fs.mkfile("P0", "/system/core.16", core16)
 
-print("[i] Install step 4 - creating userdata")
+print("[i] Install step 4 - installing Project16 shell")
+shell = requests.get(f"{base}/{vlist['Shell']['Latest']['Path']}").text
+# shell = open("shell.py", "r").read()
+fs.mkdir("P0", "/system/bin")
+fs.mkfile("P0", "/system/bin/shell", shell)
+
+print("[i] Install step 5 - creating userdata")
 fs.mkdir("P1", f"/users/{username}")
 fs.mkdir("P1", f"/appdata/{username}")
 fs.mkdir("P1", "/bin")
@@ -38,17 +46,18 @@ userlist = {
 }
 fs.mkfile("P0", "/system/config/userlist", json.dumps(userlist))
 
-print("[i] Install step 5 - installing Platinum")
-platinum_code = requests.get(f"{base}/things/platinum/platinum_a10.py").text
+print("[i] Install step 6 - installing Platinum")
+platinum_code = requests.get(f"{base}/{vlist['Platinum']['Latest']['Path']}").text
 fs.mkfile("P1", "/bin/platinum", platinum_code)
 
-print("[i] Install step 6 - adding final touches")
+print("[i] Install step 7 - adding final touches")
+fs.mkdir("P0", "/system/bin")
 systemver = {
     "OSName": "Project16",
-    "OSVersion": "A10",
-    "OSFullName": "Project16 Alpha 1.0",
+    "OSVersion": f"{vlist['Shell']['Latest']['Version']}",
     "OSInstallDate": int(time.time()),
-    "OSOwner": username
+    "OSOwner": username,
+    "Shell": "/system/bin/shell@P0"
 }
 fs.mkfile("P0", "/system/config/system.ini", json.dumps(systemver))
 bootargs = {
@@ -71,7 +80,7 @@ bootargs = {
 }
 fs.mkfile("P0", "/system/config/boot.cfg", json.dumps(bootargs))
 
-print("[i] Install step 7 - exporting FS")
+print("[i] Install step 8 - exporting FS")
 fs.exportfs(file=f"Project16.rfs")
 
 print("[i] Project16 installation finished!")
